105def8e710f41e0ab43e0b7a5a173a4693ca1a0
kiruba <kirubasankar068@gmail.com>
2025-12-23 14:24:13 +0530

    Update API base URL for production environment

--- e-commerce-frontend/src/Services/Api.js
+++ e-commerce-frontend/src/Services/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000"; // change to your backend
+const BASE_URL = "https://event-photo-gallery.onrender.com"; // change to your backend
 
 const api = axios.create({
   baseURL: BASE_URL,
